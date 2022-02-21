6717dcc117c8549e7a31cd236de73283bb4fd347
Franckyroquai <franckyroquai@gmail.com>
2022-02-21 19:07:49 +0100

    creation module d'authentification

---
+++ auth/auth.js
@@ -0,0 +1,73 @@
+const passport = require('passport');
+const localStrategy = require('passport-local').Strategy;
+const JWTstrategy = require('passport-jwt').Strategy;
+const ExtractJWT = require('passport-jwt').ExtractJwt;
+const UserModel = require('../models/model');
+
+passport.use(
+    'signup',
+    new localStrategy(
+      {
+        usernameField: 'email',
+        passwordField: 'password'
+      },
+      async (email, password, done) => {
+        console.log("mimou");
+        try {
+          console.log("signup log");
+          const user = await UserModel.create({ email, password });
+  
+          return done(null, user);
+        } catch (error) {
+          done(error);
+        }
+      }
+    )
+  );
+
+passport.use(
+    'login',
+    new localStrategy(
+      {
+        usernameField: 'email',
+        passwordField: 'password'
+      },
+      async (email, password, done) => {
+        try {
+          const user = await UserModel.findOne({ email });
+  
+          if (!user) { // true === !false >>> si pas de user avec cet email alors je passe dans cette condition
+            return done(null, false, { message: 'User not found' });
+          }
+  
+          const validate = await user.isValidPassword(password);
+  
+          if (!validate) {
+            return done(null, false, { message: 'Wrong Password' });
+          }
+  
+          return done(null, user, { message: 'Logged in Successfully' });
+        } catch (error) {
+          return done(error);
+        }
+      }
+    )
+  );
+
+  passport.use(
+    new JWTstrategy(
+      {
+        secretOrKey: 'la cle de hashage du JWT (ULTRA TOP SECRET)',
+        jwtFromRequest: ExtractJWT.fromUrlQueryParameter('secret_token')
+      },
+      async (token, done) => {
+          console.log("tok", token);
+        try {
+          return done(null, token.user);
+        } catch (error) {
+          done(error);
+        }
+      }
+    )
+  );
+  
