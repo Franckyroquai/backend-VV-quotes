bdc091f72439138108058c3ace84615c400cd539
Franckyroquai <franckyroquai@gmail.com>
2022-03-09 18:48:24 +0100

    workflow

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,13 +1,11 @@
-const logger = require("../helpers/logger");
+// const logger = require("../helpers/logger");
 const jwt = require("express-jwt");
 
 module.exports.jwtMiddleware = function (opts) {
-  logger.debug("<<-->>");
   const baseJwtConfig = {
     secret: process.env.JWT_SECRET,
     algorithms: ["HS256"],
   };
-  logger.debug(baseJwtConfig);
   if (opts) {
     return jwt(opts);
   }
