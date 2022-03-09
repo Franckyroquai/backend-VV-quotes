61d53fc133dc35e3278ade22a4e63c0876072d4a
Pierre POMET <pierre.pomet.pro@gmail.com>
2022-03-09 16:34:54 +0100

    MOD: refacto server.js

--- server.js
+++ server.js
@@ -1,35 +1,26 @@
 const dotenv = require("dotenv").config();
+const logger = require("./helpers/logger");
 if (dotenv.error) {
-  console.error(dotenv.error);
+  logger.error(dotenv.error);
   throw dotenv.error;
 }
 const express = require("express");
-const mongoose = require("mongoose");
-const passport = require("passport");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const errorHandler = require("./middlewares/errorHandler");
+const { jwtMiddleware } = require("./middlewares/authentication");
 
 const UserModel = require("./models/user");
 
 const port = process.env.SERVERPORT;
 
-const dbusername = process.env.DBUSER;
-const dbpassword = process.env.DBPASS;
-const dbcluster = process.env.DBCLUSTER;
-const dbname = process.env.DBNAME;
-
-mongoose.connect(
-  `mongodb+srv://${dbusername}:${dbpassword}@${dbcluster}.mongodb.net/${dbname}?retryWrites=true&w=majority`,
-  {}
-);
-mongoose.connection.on("error", (error) => console.error(error));
-mongoose.Promise = global.Promise;
-
-require("./authentification");
+require("./services/db");
 
 const publicAuthRoutes = require("./routes/user/public-routes");
 const privateAuthRoutes = require("./routes/user/private-routes");
 const publicQuoteRoutes = require("./routes/quote/public-routes");
+const privateQuoteRoutes = require("./routes/quote/private-routes");
+const jwt = require("express-jwt");
 
 const app = express();
 
@@ -39,23 +30,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use("/", publicAuthRoutes); // '/' >>> root ou racine
+app.use("/user", jwtMiddleware(), privateAuthRoutes);
 app.use("/quote", publicQuoteRoutes);
-
-// Plug in the JWT strategy as a middleware so only verified users can access this route.
-app.use(
-  "/user",
-  passport.authenticate("jwt", { session: false }),
-  privateAuthRoutes
-);
+app.use("/quote", jwtMiddleware(), privateQuoteRoutes);
 
 // Handle errors.
-app.use((err, req, res, next) => {
-  console.error("erreur", err);
-  res.status(err.status || 500);
-  res.json({ error: err }); // mouais pas trop secure
-  //   res.json({ error: "il y a eu une erreur dans le serveur" }); // mieux
-});
+app.use(errorHandler);
 
 app.listen(port, () => {
-  console.info(`Server started on port ${port}.`);
+  logger.info(`Server started on port ${port}.`);
 });
