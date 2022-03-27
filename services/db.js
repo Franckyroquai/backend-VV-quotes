0ba7509a310a48ead862a764dbdeb783298f755d
Franckyroquai <franckyroquai@gmail.com>
2022-03-27 17:43:05 +0200

    ajout de commentaires

--- services/db.js
+++ services/db.js
@@ -1,24 +1,25 @@
 const mongoose = require("mongoose");
 const logger = require("../helpers/logger");
 
-const dbusername = process.env.DBUSER;
-const dbpassword = process.env.DBPASS;
-const dbcluster = process.env.DBCLUSTER;
-const dbname = process.env.DBNAME;
+const dbUserName = process.env.DBUSER;
+const dbPassword = process.env.DBPASS;
+const dbCluster = process.env.DBCLUSTER;
+const dbName = process.env.DBNAME;
 
 mongoose.connect(
-  `mongodb+srv://${dbusername}:${dbpassword}@${dbcluster}.mongodb.net/${dbname}?retryWrites=true&w=majority`,
+  `mongodb+srv://${dbUserName}:${dbPassword}@${dbCluster}.mongodb.net/${dbName}?retryWrites=true&w=majority`,
   {}
 );
 mongoose.connection.on("error", (error) => {
+  //on ecouteur d'événement de connexion mongoose
   logger.error("mongoose connection error", error);
   process.exit;
 });
 mongoose.connection.on("connected", () => {
   logger.info("MongoDb via Mongoose is connected");
 });
-mongoose.connection.on("disconnected", (something) => {
-  logger.info("mongoose disconnected", something);
+mongoose.connection.on("disconnected", (reason) => {
+  logger.info("mongoose disconnected", reason);
 });
 
-mongoose.Promise = global.Promise;
+mongoose.Promise = global.Promise; //prommesses mongoose mêmes promesses que le reste du systeme (async await)
