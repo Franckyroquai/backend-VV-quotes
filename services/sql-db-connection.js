8ab2ae16dd406b9f27546e8d9dde319f1632df4d
Franckyroquai <franckyroquai@gmail.com>
2022-04-27 20:28:44 +0200

    creation dossier pour les services

---
+++ services/sql-db-connection.js
@@ -0,0 +1,30 @@
+const { Sequelize } = require("sequelize");
+const logger = require("../helpers/logger");
+
+const sequelize = new Sequelize("testdatavv", "root", "", {
+  host: "localhost",
+  dialect: "mysql",
+  logging: true,
+});
+
+async function testConnection() {
+  try {
+    await sequelize.authenticate();
+    logger.info("Mysql database connection has been established successfully.");
+  } catch (error) {
+    logger.error("Unable to connect to the sql database:", error);
+  }
+  // try {
+  //   await sequelize.close();
+  //   logger.debug("mysql connection closed");
+  // } catch (error) {
+  //   logger.error(
+  //     "closing connection with mysql DB encountered an error:",
+  //     error
+  //   );
+  // }
+}
+
+testConnection();
+
+module.exports = sequelize;
