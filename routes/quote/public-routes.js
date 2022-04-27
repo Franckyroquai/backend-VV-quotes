54c89a11c3c548667fc51189550a098bd40aa017
Franckyroquai <franckyroquai@gmail.com>
2022-04-27 20:20:23 +0200

    MAJ count, all, random-js

--- routes/quote/public-routes.js
+++ routes/quote/public-routes.js
@@ -1,69 +1,40 @@
 const express = require("express");
 const logger = require("../../helpers/logger");
-const quoteModel = require("../../models/quote");
 const { randomIntFromInterval } = require("../../helpers/math");
+const { QuoteModel } = require("../../models/sql-quote");
 
 const router = express.Router();
 
 router.get("/random-js", async (req, res) => {
   try {
-    const quoteList = await quoteModel.find({});
-    const randomQuoteWithMetaData =
-      quoteList[randomIntFromInterval(0, quoteList.length - 1)];
-    const randomQuote = {
-      text: randomQuoteWithMetaData.text,
-      author: randomQuoteWithMetaData.author,
-    };
-    res.json(randomQuote);
+    var allQuotesArray = await QuoteModel.findAll({});
+
+    var randomIdx = randomIntFromInterval(0, allQuotesArray.length - 1);
+
+    var randomQuote = allQuotesArray[randomIdx];
+
+    res.json({
+      text: randomQuote.dataValues.text,
+      author: randomQuote.dataValues.author,
+    });
   } catch (err) {
-    logger.log("erreur:", err.message);
-    res.status(500).send("error");
+    logger.error("/random-js server error");
+    res.status(500).json({ message: "internal server error" });
   }
-});
-
-//obtenir une citation par une méthode mongoose
-router.get("/random-mongoose", async (req, res) => {
-  const total = await quoteModel.estimatedDocumentCount();
-  const randomQouteWithMetaData = await quoteModel
-    .findOne({})
-    .skip(randomIntFromInterval(0, total - 1))
-    .exec();
-
-  const randomQuote = {
-    text: randomQouteWithMetaData.text,
-    author: randomQouteWithMetaData.author,
-  };
-
-  logger.log(randomQouteWithMetaData);
-  logger.info(quoteModel.quoteCleanUp(randomQouteWithMetaData));
-
-  res.json(randomQuote);
-});
-
-//obtenir une citation par une librairie mongoose
-router.get("/random-mongoose-lib", async (req, res) => {
-  quoteModel.findOneRandom((err, randomQouteWithMetaData) => {
-    logger.log(
-      "cleaned up quote:\n",
-      quoteModel.quoteCleanUp(randomQouteWithMetaData)
-    );
-    const randomQuote = {
-      text: randomQouteWithMetaData.text,
-      author: randomQouteWithMetaData.author,
-    };
-    res.json(randomQuote);
-  });
 });
 
 //Obtenir la liste de toutes les citations
 router.get("/all", async (req, res) => {
-  const quotes = await quoteModel.find({}, ["text", "author"]);
-  res.json({ quotes });
+  var allQuotes = await QuoteModel.findAll({
+    attributes: ["text", "author"],
+  });
+  logger.debug(allQuotes);
+  res.json(allQuotes);
 });
 
 router.get("/count", async (req, res) => {
-  const count = await quoteModel.estimatedDocumentCount();
-  res.json({ count });
+  var quoteArray = await QuoteModel.findAll();
+  res.json({ count: quoteArray.length });
 });
 
 module.exports = router;
