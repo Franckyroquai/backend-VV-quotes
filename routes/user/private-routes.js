42862428e64cfc2fdf5bf5356aa6208de497285d
Franckyroquai <franckyroquai@gmail.com>
2022-05-05 22:16:14 +0200

    wip

--- routes/user/private-routes.js
+++ routes/user/private-routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const logger = require("../../helpers/logger");
-const { UserModel } = require("../../models/sql-user");
+const { UserModel } = require("../../models/user");
 const router = express.Router();
 
 router.get("/profile", async (req, res) => {});
