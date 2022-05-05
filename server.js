143aa2e0a049bee12fce4a070b0d349ec0de9fb1
Franckyroquai <franckyroquai@gmail.com>
2022-05-05 22:17:20 +0200

    wip

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const cors = require("cors"); //permet d'outrepasser une faille de sécurité en
 const errorHandler = require("./middlewares/errorHandler");
 const { jwtMiddleware } = require("./middlewares/authentication");
 
-require("./services/sql-db-connection");
-require("./services/sql-db-init");
+require("./services/db-connection");
+require("./services/db-init");
 
 const app = express();
 
