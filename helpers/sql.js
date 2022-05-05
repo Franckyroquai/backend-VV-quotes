6895081f28fd726b83a28aaf4a3f091d70917f7b
Franckyroquai <franckyroquai@gmail.com>
2022-05-05 22:15:04 +0200

    ajout type of sync

--- helpers/sql.js
+++ helpers/sql.js
@@ -10,23 +10,23 @@ async function syncronize(model, whichOne) {
   //   throw new Error("texte d'erreur adapté");
   // }
   logger.warn("model: ", whichOne);
-  logger.error("--- model infos ---");
-  logger.debug(model);
+  // logger.error("--- model infos ---");
+  // logger.debug(model);
   // logger.debug(typeof model);
   // // logger.debug(instanceof model);
   // logger.debug(model.constructor);
   // logger.debug(model.constructor.name);
   // logger.debug();
-  logger.error("--- model infos ---");
+  // logger.error("--- model infos ---");
   if (typeOfSync && typeof typeOfSync === "string") {
     if (typeOfSync === "force") {
-      logger.warn("type of sync : force");
+      // logger.warn("type of sync : force");
       return await model.sync({ force: true });
     } else if (typeOfSync === "alter") {
-      logger.warn("alter");
+      // logger.warn("alter");
       return await model.sync({ alter: true });
     } else if (typeOfSync === "none") {
-      logger.warn("none");
+      // logger.warn("none");
       return await model.sync();
     } else {
       throw new Error("Bad Env Parameter for DB_SYNC_TYPE");
