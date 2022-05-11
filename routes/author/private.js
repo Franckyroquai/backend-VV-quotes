5cf4ad0fb8ddeb58bb2bb971dcd1495c176fa835
Franckyroquai <franckyroquai@gmail.com>
2022-05-11 22:28:10 +0200

    creation routes/author

---
+++ routes/author/private.js
@@ -0,0 +1,52 @@
+if (process.env.NODE_ENV === "dev") {
+  //TODO:DRY
+  var casual = require("casual"); //Fake data generator
+}
+const express = require("express");
+const router = express.Router();
+const logger = require("../../helpers/logger");
+const { randomIntFromInterval } = require("../../helpers/math");
+const { AuthorModel } = require("../../models/author");
+
+router.post("/create-one", async (req, res) => {
+  var author = req.body;
+  try {
+    if (!author.name || author.name === "") {
+      //TODO: penser à vérifier la casse de la string Anonyme avec le front
+      author.name = "Anonyme";
+    } else {
+      var CreatedAuthor = await AuthorModel.create({
+        name: author.name,
+        wikilink: author.wikilink,
+      });
+      res.json({ author: CreatedAuthor });
+    }
+  } catch (err) {
+    logger.error(err);
+    res.status(500).json({ message: "unknown server error" });
+  }
+});
+
+router.post("/mock-one", async (req, res) => {
+  try {
+    var author = await AuthorModel.create({
+      name: casual.name,
+      wikilink: "https://wikipedia.com",
+    });
+    res.status(200).json({ author });
+  } catch (err) {
+    logger.error(err);
+    res.status(500).json({ message: "unknown server error" });
+  }
+});
+
+router.get("/all", async (req, res) => {
+  try {
+    var authorList = await AuthorModel.findAll({});
+    res.status(200).json({ authorList });
+  } catch (err) {
+    //faire gestion erreur
+  }
+});
+
+module.exports = router;
