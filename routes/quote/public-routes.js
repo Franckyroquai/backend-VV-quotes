bff1584ecd00a8436954f178f5cc3c0a8af38255
Franckyroquai <franckyroquai@gmail.com>
2022-05-11 22:30:41 +0200

    all/ random

--- routes/quote/public-routes.js
+++ routes/quote/public-routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("../../helpers/logger");
 const { randomIntFromInterval } = require("../../helpers/math");
+const { AuthorModel } = require("../../models/author");
 const { QuoteModel } = require("../../models/quote");
 
 const router = express.Router();
@@ -13,9 +14,19 @@ router.get("/random-js", async (req, res) => {
 
     var randomQuote = allQuotesArray[randomIdx];
 
+    logger.warn(randomQuote);
+
+    // var authorOfRandomQuote = await AuthorModel.findOne({
+    //   where: { id: randomQuote.authorId },
+    // });
+
+    var authorOfRandomQuote = await randomQuote.getAuthor();
+
+    logger.warn(authorOfRandomQuote);
+
     res.json({
       text: randomQuote.dataValues.text,
-      author: randomQuote.dataValues.author,
+      author: authorOfRandomQuote.name || "",
     });
   } catch (err) {
     logger.error("/random-js server error");
@@ -25,11 +36,27 @@ router.get("/random-js", async (req, res) => {
 
 //Obtenir la liste de toutes les citations
 router.get("/all", async (req, res) => {
-  var allQuotes = await QuoteModel.findAll({
-    attributes: ["text", "author"],
-  });
-  logger.debug(allQuotes);
-  res.json(allQuotes);
+  // var allQuotes = await QuoteModel.findAll({
+  //   attributes: ["text", "author"],
+  // });
+  // logger.debug(allQuotes);
+  // res.json(allQuotes);
+  try {
+    var allQuotes = await QuoteModel.findAll({ include: AuthorModel });
+    var newArray = [];
+    for (var idx = 0; idx < allQuotes.length; idx++) {
+      var author = await allQuotes[idx].getAuthor();
+      var authorName = author ? author.name : " Anonyme";
+      newArray.push({
+        text: allQuotes[idx].text,
+        authorName,
+      });
+    }
+    res.json(newArray);
+  } catch (err) {
+    logger.error(err);
+    res.status(500).json({ message: "internal server error" });
+  }
 });
 
 router.get("/count", async (req, res) => {
