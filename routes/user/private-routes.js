16e74fdb6ff6460f203743e9d3cd2f48f30b79f6
Franckyroquai <franckyroquai@gmail.com>
2022-05-15 15:07:59 +0200

    liste fonction à prévoir

--- routes/user/private-routes.js
+++ routes/user/private-routes.js
@@ -3,6 +3,18 @@ const logger = require("../../helpers/logger");
 const { UserModel } = require("../../models/user");
 const router = express.Router();
 
+router.post("/create-one", async (req, res) => {
+  res.send("todo");
+});
+
+router.post("/update", async (req, res) => {
+  res.send("todo");
+});
+
+router.post("/authorization", async (req, res) => {
+  res.send("todo");
+});
+
 router.get("/profile", async (req, res) => {});
 
 router.delete("/delete", async (req, res) => {});
