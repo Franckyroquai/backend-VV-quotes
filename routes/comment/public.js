9d5c2cfea6712f1a429cda92e53d41c681bbc9d2
Franckyroquai <franckyroquai@gmail.com>
2022-05-16 16:46:49 +0200

    modif synthaxe todo

--- routes/comment/public.js
+++ routes/comment/public.js
@@ -5,7 +5,7 @@ const logger = require("../../helpers/logger");
 const { CommentModel } = require("../../models/comment");
 
 router.get("all", async (req, res) => {
-  res.send("todo");
+  res.send("todo"); //TODO: to implement
 });
 
 module.exports = router;
