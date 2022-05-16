cce46ab06369d33aebacb945f57147da908bde63
Franckyroquai <franckyroquai@gmail.com>
2022-05-16 16:46:58 +0200

    modif synthaxe todo

--- routes/comment/private.js
+++ routes/comment/private.js
@@ -5,23 +5,23 @@ const logger = require("../../helpers/logger");
 const { CommentModel } = require("../../models/comment");
 
 router.post("/create-one", async (req, res) => {
-  res.send("todo");
+  res.send("todo");//TODO: to implement
 });
 
 router.post("/update-one", async (req, res) => {
-  res.send("todo");
+  res.send("todo");//TODO: to implement
 });
 
 router.post("/delete-one", async (req, res) => {
-  res.send("todo");
+  res.send("todo");//TODO: to implement
 });
 
 router.post("/link", async (req, res) => {
-  res.send("todo");
+  res.send("todo");//TODO: to implement
 });
 
 router.post("/validate", async (req, res) => {
-  res.send("todo");
+  res.send("todo");//TODO: to implement
 });
 
 module.exports = router;
