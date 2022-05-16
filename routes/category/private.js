f99f961e4834c310876ffaf4ce03bd328bc5a7c4
Franckyroquai <franckyroquai@gmail.com>
2022-05-16 16:47:19 +0200

    modif synthaxe todo

--- routes/category/private.js
+++ routes/category/private.js
@@ -5,19 +5,19 @@ const logger = require("../../helpers/logger");
 const { CategoryModel } = require("../../models/category");
 
 router.post("/create-one", async (req, res) => {
-  res.send("todo");
+  res.send("todo");//TODO: to implement
 });
 
 router.post("/update-one", async (req, res) => {
-  res.send("todo");
+  res.send("todo");//TODO: to implement
 });
 
 router.post("/delete-one", async (req, res) => {
-  res.send("todo");
+  res.send("todo");//TODO: to implement
 });
 
 router.post("/link", async (req, res) => {
-  res.send("todo");
+  res.send("todo");//TODO: to implement
 });
 
 module.exports = router;
