43aeb1441119e8ded01e0de8a6cc68f36273148e
Franckyroquai <franckyroquai@gmail.com>
2022-05-16 16:44:59 +0200

    creation get all post

--- routes/post/public.js
+++ routes/post/public.js
@@ -5,7 +5,13 @@ const logger = require("../../helpers/logger");
 const { PostModel } = require("../../models/post");
 
 router.get("/all", async (req, res) => {
-  res.send("todo");
+  try {
+    const allPostsArray = await PostModel.findAll();
+    res.status(200).json(allPostsArray);
+  } catch (error) {
+    logger.debug("error in get all posts");
+    res.status(500).json("server error");
+  }
 });
 
 module.exports = router;
