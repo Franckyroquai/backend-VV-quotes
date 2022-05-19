e65af9101a4cb7a25ac7243283042224968691f5
Franckyroquai <franckyroquai@gmail.com>
2022-05-19 19:42:02 +0200

    wip

--- routes/comment/public.js
+++ routes/comment/public.js
@@ -5,7 +5,13 @@ const logger = require("../../helpers/logger");
 const { CommentModel } = require("../../models/comment");
 
 router.get("all", async (req, res) => {
-  res.send("todo"); //TODO: to implement
+  try {
+    const allCommentsArray = await CommentModel.findAll();
+    res.status(200).json(allCommentsArray);
+  } catch (error) {
+    logger.debug("error in get all comments");
+    res.status(500).json("server error");
+  }
 });
 
 module.exports = router;
