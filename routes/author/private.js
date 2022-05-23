a956e761401b762be7918b59be651d394d71c8de
Franckyroquai <franckyroquai@gmail.com>
2022-05-23 20:50:28 +0200

    suppression commentaires

--- routes/author/private.js
+++ routes/author/private.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV === "dev") {
   //TODO:DRY
-  var casual = require("casual"); //Fake data generator
+  var casual = require("casual");
 }
 const express = require("express");
 const router = express.Router();
@@ -50,19 +50,19 @@ router.get("/all", async (req, res) => {
 });
 
 router.get("/one", async (req, res) => {
-  res.send("todo");//TODO: to implement
+  res.send("todo"); //TODO: to implement
 });
 
 router.post("/update", async (req, res) => {
-  res.send("todo");//TODO: to implement
+  res.send("todo"); //TODO: to implement
 });
 
 router.post("/delete", async (req, res) => {
-  res.send("todo");//TODO: to implement
+  res.send("todo"); //TODO: to implement
 });
 
 router.post("/link", async (req, res) => {
-  res.send("todo");//TODO: to implement
+  res.send("todo"); //TODO: to implement
 });
 
 module.exports = router;
