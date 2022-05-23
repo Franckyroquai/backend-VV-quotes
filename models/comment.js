2280926a1a6c2f5d99e1d68d7d3ef02934510ed1
Franckyroquai <franckyroquai@gmail.com>
2022-05-23 20:51:12 +0200

    suppression commentaires

--- models/comment.js
+++ models/comment.js
@@ -2,31 +2,25 @@ const { DataTypes } = require("sequelize");
 const sequelize = require("../services/db-connection");
 const logger = require("../helpers/logger");
 
-const Comment = sequelize.define(
-  "comment",
-  {
-    text: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      required: true,
-    },
-    image: {
-      type: DataTypes.BLOB,
-      allowNull: true,
-      required: false,
-    },
-    createdAt: {
-      type: DataTypes.DATE,
-      allowNull: false,
-    },
-    updatedAt: {
-      type: DataTypes.DATE,
-      allowNull: false,
-    },
+const Comment = sequelize.define("comment", {
+  text: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    required: true,
   },
-  {
-    // Other model options go here
-  }
-);
+  image: {
+    type: DataTypes.BLOB,
+    allowNull: true,
+    required: false,
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+});
 
 module.exports = { CommentModel: Comment };
