8be6264767c3475f269bcaeec542756ada325b81
Franckyroquai <franckyroquai@gmail.com>
2022-05-23 20:50:52 +0200

    suppression commentaires

--- models/quote.js
+++ models/quote.js
@@ -2,24 +2,15 @@ const { DataTypes } = require("sequelize");
 const sequelize = require("../services/db-connection");
 const logger = require("../helpers/logger");
 
-const Quote = sequelize.define(
-  "quote",
-  {
-    // Model attributes are defined here
-    text: {
-      type: DataTypes.STRING(8000),
-      allowNull: false,
-      //TODO:vérifier unique: true;
-    },
-
-    //TODO:insérer le champ "timestamps"
+const Quote = sequelize.define("quote", {
+  // Model attributes are defined here
+  text: {
+    type: DataTypes.STRING(8000),
+    allowNull: false,
+    //TODO:vérifier unique: true;
   },
-  {
-    // Other model options go here
-  }
-);
 
-// Quote.hasOne(AuthorModel, { foreignKey: { name: "myQuoteId" } });
-// AuthorModel.belongsToMany(Quote);
+  //TODO:insérer le champ "timestamps"
+});
 
 module.exports = { QuoteModel: Quote };
