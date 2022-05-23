13715d13461ec8465c99615534c8b1e27c713806
Franckyroquai <franckyroquai@gmail.com>
2022-05-23 20:51:34 +0200

    suppression commentaires

--- models/author.js
+++ models/author.js
@@ -2,30 +2,20 @@ const { DataTypes } = require("sequelize");
 const sequelize = require("../services/db-connection");
 const logger = require("../helpers/logger");
 
-const Author = sequelize.define(
-  "author",
-  {
-    // Model attributes are defined here
-    name: {
-      type: DataTypes.STRING(8000),
-      allowNull: false, //TODO: vérifier
-      //TODO:vérifier unique: true;
-    },
-    wikilink: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      // allowNull defaults to true
-    },
-
-    //TODO:insérer le champ "timestamps"
+const Author = sequelize.define("author", {
+  name: {
+    type: DataTypes.STRING(8000),
+    allowNull: false, //TODO: vérifier
+    //TODO:vérifier unique: true;
   },
-  {
-    // Other model options go here
-  }
-);
+  wikilink: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    // allowNull defaults to true
+  },
 
-// Author.hasMany(QuoteModel);
-// QuoteModel.belongsTo(Author);
+  //TODO:insérer le champ "timestamps"
+});
 
 module.exports = {
   AuthorModel: Author,
