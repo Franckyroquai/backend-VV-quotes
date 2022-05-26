c0a93d254d24bdd7249d8e69f86e5197f0e34cb8
Franckyroquai <franckyroquai@gmail.com>
2022-05-26 15:05:32 +0200

    maj TODO

--- routes/contact-info/private.js
+++ routes/contact-info/private.js
@@ -1,27 +1,27 @@
-const express = require("express");
-const router = express.Router();
-const logger = require("../../helpers/logger");
+var express = require("express");
+var router = express.Router();
+var logger = require("../../helpers/logger");
 
-const { ContactInfoModel } = require("../../models/contact-info");
+var { ContactInfoModel } = require("../../models/contact-info");
 
 router.post("/create-one", async (req, res) => {
-  res.send("todo");//TODO: to implement
+  res.send("todo"); //TODO: to implement
 });
 
 router.post("/update-one", async (req, res) => {
-  res.send("todo");//TODO: to implement
+  res.send("todo"); //TODO: to implement
 });
 
 router.post("/delete-one", async (req, res) => {
-  res.send("todo");//TODO: to implement
+  res.send("todo"); //TODO: to implement
 });
 
 router.post("/link", async (req, res) => {
-  res.send("todo");//TODO: to implement
+  res.send("todo"); //TODO: to implement
 });
 
 router.post("/validate", async (req, res) => {
-  res.send("todo");//TODO: to implement
+  res.send("todo"); //TODO: to implement for pepo
 });
 
 module.exports = router;
