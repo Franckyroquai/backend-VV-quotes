92944fdf3d58e69f440fdf50282a4bace32b1d88
Franckyroquai <franckyroquai@gmail.com>
2022-05-26 15:08:13 +0200

    wip

--- routes/author/public.js
+++ routes/author/public.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const router = express.Router();
-const logger = require("../../helpers/logger");
+var express = require("express");
+var router = express.Router();
+var logger = require("../../helpers/logger");
 
 module.exports = router;
