ca1588fd6623d6b5030d7f1d53ca83bacf3fea96
Franckyroquai <franckyroquai@gmail.com>
2022-05-26 15:07:49 +0200

    suppression fonctions link

--- routes/category/private.js
+++ routes/category/private.js
@@ -1,23 +1,96 @@
-const express = require("express");
-const router = express.Router();
-const logger = require("../../helpers/logger");
+var express = require("express");
+const { getRandIdFromModel } = require("../../helpers/dev");
+var router = express.Router();
+var logger = require("../../helpers/logger");
 
-const { CategoryModel } = require("../../models/category");
+var casual = require("casual");
 
-router.post("/create-one", async (req, res) => {
-  res.send("todo");//TODO: to implement
+var { CategoryModel } = require("../../models/category");
+
+function sanitizedCreateCategoryObject(request) {
+  var sanitizedObject = {};
+  var requestBody = request.body;
+  if (Object.keys(requestBody).length === 0) {
+    return false;
+  }
+  if (
+    !requestBody.CategoryName ||
+    !(typeof requestBody.CategoryName === "string") ||
+    !(requestBody.CategoryName.length >= 1)
+  ) {
+    return false;
+  }
+  Object.assign(sanitizedObject, { CategoryName: requestBody.CategoryName });
+
+  return sanitizedObject;
+}
+
+router.post("/create", async (req, res) => {
+  try {
+    var sanitizedCategoryObject = sanitizedCreateCategoryObject(req);
+    if (!sanitizedCategoryObject.error) {
+      var newCategory = await CategoryModel.create(sanitizedCategoryObject);
+      res.status(200).json(newCategory);
+    } else {
+      res.status(500).json({ message: "bad request" });
+    }
+  } catch (error) {
+    logger.warn(error);
+    res.status(500).json({ message: "server error" });
+  }
 });
 
-router.post("/update-one", async (req, res) => {
-  res.send("todo");//TODO: to implement
+router.post("/update", async (req, res) => {
+  try {
+    var sanitized = sanitizedCreateCategoryObject(req);
+    var CategoryToUpdate = await CategoryModel.findOne({
+      where: { id: req.body.id },
+    });
+    var updatedCategory = await CategoryToUpdate.update(sanitized);
+    res.status(200).json(updatedCategory);
+  } catch (error) {
+    logger.warn(error);
+    res.status(500).json({ message: "server error" });
+  }
 });
 
-router.post("/delete-one", async (req, res) => {
-  res.send("todo");//TODO: to implement
+router.delete("/delete", async (req, res) => {
+  try {
+    var idToDestroy = req.body.id;
+    var categoryToDestroy = await CategoryModel.findOne({
+      where: { id: idToDestroy },
+    });
+    if (!categoryToDestroy) {
+      res
+        .status(404)
+        .json({ message: "category to destroy not found", id: idToDestroy });
+    } else {
+      var destroyedCategory = await categoryToDestroy.destroy();
+      res.status(200).json({ id: destroyedCategory.id, destroyed: true });
+    }
+  } catch (error) {
+    logger.debug("server error");
+    logger.error(error);
+    res.status(500).json({ message: "server error" });
+  }
 });
 
-router.post("/link", async (req, res) => {
-  res.send("todo");//TODO: to implement
+router.post("/generate", async (req, res) => {
+  var generationQty = req.body.number || 5;
+  var categoryArray = [];
+  try {
+    for (var idx = 0; idx < generationQty; idx++) {
+      categoryArray.push({
+        CategoryName: casual.name,
+      });
+    }
+    var category = await CategoryModel.bulkCreate(categoryArray);
+    logger.info("category >>", category.length, "<<");
+    res.status(200).json({ ok: true, number: category.length });
+  } catch (err) {
+    logger.error(err);
+    res.json({ ok: false });
+  }
 });
 
 module.exports = router;
