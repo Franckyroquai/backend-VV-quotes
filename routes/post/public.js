f36e6060774782c575ef2433c65f380ae8a2702a
Franckyroquai <franckyroquai@gmail.com>
2022-05-26 15:04:19 +0200

    wip

--- routes/post/public.js
+++ routes/post/public.js
@@ -1,12 +1,12 @@
-const express = require("express");
-const router = express.Router();
-const logger = require("../../helpers/logger");
+var express = require("express");
+var router = express.Router();
+var logger = require("../../helpers/logger");
 
-const { PostModel } = require("../../models/post");
+var { PostModel } = require("../../models/post");
 
 router.get("/all", async (req, res) => {
   try {
-    const allPostsArray = await PostModel.findAll();
+    var allPostsArray = await PostModel.findAll();
     res.status(200).json(allPostsArray);
   } catch (error) {
     logger.debug("error in get all posts");
