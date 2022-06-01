ba8b6eb76a7e47c72d295866e23694725bf5fd9c
Franckyroquai <franckyroquai@gmail.com>
2022-06-01 18:18:51 +0200

    wip

--- helpers/sql.js
+++ helpers/sql.js
@@ -7,10 +7,10 @@ async function syncronize(model) {
       logger.warn("type of sync : force");
       return await model.sync({ force: true });
     } else if (typeOfSync === "alter") {
-      logger.warn("type of sync : alter");
+      // logger.warn("type of sync : alter");
       return await model.sync({ alter: true });
     } else if (typeOfSync === "none") {
-      logger.warn("type of sync : none");
+      // logger.warn("type of sync : none");
       return await model.sync();
     }
   }
