c2ea1e0105ebb1361f548c0e950432472fee1a55
Franckyroquai <franckyroquai@gmail.com>
2022-06-01 18:19:28 +0200

    maj authorization

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -3,50 +3,53 @@ const routeConfigs = require("../config/authorization.json");
 
 module.exports = function (req, res, next) {
   // si l'utilisateur est admin il a forcement le droit de passer
+  logger.warn(req.user);
+  logger.warn(req.user.type);
   if (req.user.type === "admin") {
-    // logger.debug("bypassing authorization middleware");
+    logger.debug("bypassing authorization middleware");
     next();
-  }
-  // logger.warn(Object.keys(req));
-  // logger.debug("req.url", req.url);
-  // logger.error("------------------------------------------------");
-  // logger.debug("req.originalUrl", req.originalUrl);
-  // logger.info("JSON de Config des droits", routeConfigs);
-  logger.warn(routeConfigs.dev.author.generate);
-  logger.warn(routeConfigs["dev"]["author"]["generate"]);
-  var truc = routeConfigs["dev"];
-  truc = truc["author"];
-  truc = truc["generate"];
-  logger.debug(truc);
-  // logger.info(req.originalUrl.substring(1));
-  // logger.warn(req.originalUrl.split("/"));
-  logger.info(req.originalUrl.substring(1).split("/"));
-  var splitUrl = req.originalUrl.substring(1).split("/");
-  // logger.error("------------------------------------------------");
-  if (req.user) {
-    //boucle d'accession au tableau d'autorisations
+  } else {
+    // logger.warn(Object.keys(req));
+    // logger.debug("req.url", req.url);
+    // logger.error("------------------------------------------------");
+    // logger.debug("req.originalUrl", req.originalUrl);
+    // logger.info("JSON de Config des droits", routeConfigs);
+    // logger.warn(routeConfigs.dev.author.generate);
+    // logger.warn(routeConfigs["dev"]["author"]["generate"]);
+    var truc = routeConfigs["dev"];
+    truc = truc["author"];
+    truc = truc["generate"];
+    // logger.debug(truc);
+    // logger.info(req.originalUrl.substring(1));
+    // logger.warn(req.originalUrl.split("/"));
+    // logger.info(req.originalUrl.substring(1).split("/"));
+    var splitUrl = req.originalUrl.substring(1).split("/");
+    // logger.error("------------------------------------------------");
+    if (req.user) {
+      //boucle d'accession au tableau d'autorisations
 
-    var value;
+      var value;
 
-    logger.debug("auth arr length", splitUrl.length);
-    for (let i = 0; i < splitUrl.length; i++) {
-      logger.debug("i", i);
-      if (i === 0) {
-        value = routeConfigs[`${splitUrl[i]}`]; // <=> value = routeConfigs.dev
-      } else {
-        value = value[`${splitUrl[i]}`]; // <=> ([0] value = value.dev [1] value = value.author [2] value = value.generate )
+      // logger.debug("auth arr length", splitUrl.length);
+      for (let i = 0; i < splitUrl.length; i++) {
+        logger.debug("i", i);
+        if (i === 0) {
+          value = routeConfigs[`${splitUrl[i]}`]; // <=> value = routeConfigs.dev
+        } else {
+          value = value[`${splitUrl[i]}`]; // <=> ([0] value = value.dev [1] value = value.author [2] value = value.generate )
+        }
+        // logger.error(`split URL at ${i} >>${splitUrl[i]}<<`);
+        // logger.warn(`value at ${i} >>${JSON.stringify(value)}<<`);
+        // logger.log("after loop value", value);
       }
-      logger.error(`split URL at ${i} >>${splitUrl[i]}<<`);
-      logger.warn(`value at ${i} >>${JSON.stringify(value)}<<`);
-      // logger.log("after loop value", value);
-    }
-    // logger.log(req.user.type);
-    if (value.find((elem) => elem === req.user.type)) {
-      // logger.debug("authorization Middleware passed");
-      next();
-    } else {
-      // logger.debug("authorization Middleware blocked");
-      res.status(403).json({ message: "forbidden" });
+      // logger.log(req.user.type);
+      if (value.find((elem) => elem === req.user.type)) {
+        logger.debug("authorization Middleware passed");
+        next();
+      } else {
+        logger.debug("authorization Middleware blocked");
+        res.status(403).json({ message: "forbidden" });
+      }
     }
   }
 };
