6d46413ea9552927d6f6e3ab08148d43a7b160f3
Franckyroquai <franckyroquai@gmail.com>
2022-06-22 21:44:10 +0200

    maj routes post

--- routes/post/index.js
+++ routes/post/index.js
@@ -21,10 +21,22 @@ const postCreate = require("./private/create");
 const postDelete = require("./private/delete");
 const postUpdate = require("./private/update");
 const postValidate = require("./private/validate");
+const postLinkCategory = require("./private/link-category");
+const postUnlinkCategory = require("./private/unlink-category");
 
 router.use("/create", [Authentication(), Authorization], postCreate);
 router.use("/delete", [Authentication(), Authorization], postDelete);
 router.use("/update", [Authentication(), Authorization], postUpdate);
 router.use("/validate", [Authentication(), Authorization], postValidate);
+router.use(
+  "/link-category",
+  [Authentication(), Authorization],
+  postLinkCategory
+);
+router.use(
+  "/unlink-category",
+  [Authentication(), Authorization],
+  postUnlinkCategory
+);
 
 module.exports = { postRouter: router, postDevRouter: devRouter };
