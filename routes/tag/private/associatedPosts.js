59c397ca305b82684b59c693d1f1b038e4a8592b
Franckyroquai <franckyroquai@gmail.com>
2022-06-25 19:39:58 +0200

    wip

--- routes/tag/private/associatedPosts.js
+++ routes/tag/private/associatedPosts.js
@@ -2,7 +2,7 @@ var router = require("express").Router();
 var logger = require("../../../helpers/logger");
 var { TagModel } = require("../../../models/tag");
 
-module.exports = router.post("/", async (req, res) => {
+module.exports = router.get("/", async (req, res) => {
   try {
     // logger.debug(Object.keys(TagModel));
     // logger.debug(TagModel.associations);
