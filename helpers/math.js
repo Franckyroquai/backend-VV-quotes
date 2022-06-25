98c9f940084d05bb4c5e2dc9a4dc7a3fa442ef95
Franckyroquai <franckyroquai@gmail.com>
2022-06-25 19:46:00 +0200

    wip

--- helpers/math.js
+++ helpers/math.js
@@ -8,13 +8,13 @@ function trueBiasedRandomBoolean(biasPercent) {
   }
   let bias = biasPercent / 100;
   var result = Math.random();
-  // console.log("biased Boolean", result);
   return result < bias;
 }
 
 function checkIfId(id) {
-  return typeof id === "number" && parseInt(id) == id && id > 0;
+  return typeof id === "number" && parseInt(id, 10) == id && id > 0;
 }
+
 module.exports = {
   randomIntFromInterval,
   trueBiasedRandomBoolean,
