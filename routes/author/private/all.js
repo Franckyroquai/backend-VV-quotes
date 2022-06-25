d63e8f5acaa92f94c1ba43a7333c2b6fe17125d9
Franckyroquai <franckyroquai@gmail.com>
2022-06-25 19:43:40 +0200

    wip

--- routes/author/private/all.js
+++ routes/author/private/all.js
@@ -2,7 +2,7 @@ var router = require("express").Router();
 var logger = require("../../../helpers/logger");
 var { AuthorModel } = require("../../../models/author");
 
-module.exports = router.post("/", async (req, res) => {
+module.exports = router.get("/", async (req, res) => {
   try {
     var authorList = await AuthorModel.findAll({});
     res.status(200).json({ authorList }); //FIXME: interface contract
