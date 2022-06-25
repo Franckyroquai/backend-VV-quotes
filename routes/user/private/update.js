5916ba8cf8fd3a63b39b16912a245765a8486f1b
Franckyroquai <franckyroquai@gmail.com>
2022-06-25 19:39:07 +0200

    wip

--- routes/user/private/update.js
+++ routes/user/private/update.js
@@ -1,26 +1,48 @@
 var router = require("express").Router();
 var logger = require("../../../helpers/logger");
+const { checkIfId } = require("../../../helpers/math");
 var { UserModel } = require("../../../models/user");
 
-function verifyUserInfos(userObject) {
-  if (userObject && typeof userObject === "object") {
-    if (userObject.email && userObject.password && userObject.type) {
-      return true;
+function sanitizer(request) {
+  if (checkIfId(request.body.id)) {
+    if (request.body.email && typeof request.body.email != "string") {
+      logger.debug("bad email");
+      return false;
     }
+    if (
+      request.body.password &&
+      (typeof request.body.password != "string" ||
+        request.body.password.length < 6)
+    ) {
+      logger.debug("bad password");
+      return false;
+    }
+    return true;
+  } else {
+    logger.debug("bad else");
+
+    return false;
+  }
+}
+
+function checkUserRights(request) {
+  var userIdToModify = request.body.id;
+  var requestUser = request.user;
+  if (requestUser.type === "admin" || userIdToModify === requestUser.id) {
+    return true;
   }
   return false;
 }
 
 module.exports = router.post("/", async (req, res) => {
   try {
-    if (verifyUserInfos(req.body) && req.body.id) {
+    if (sanitizer(req) && checkUserRights(req)) {
       var user = await UserModel.findOne({
         where: { id: req.body.id },
       });
       var updatedUser = await user.update({
         email: req.body.email,
         password: req.body.password,
-        type: req.body.type,
       });
       res.status(200).json({ user: updatedUser, message: "updated" });
     } else {
